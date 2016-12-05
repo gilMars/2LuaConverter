2aa5ae04b798271d58b8be692c3d9b81bc973cf6
gilMars <gilmar.lilium@gmail.com>
2016-12-05 11:40:58 -0300

    Transformada em classe filha da classe GeneralItem

--- include/itemname.hpp
+++ include/itemname.hpp
@@ -2,21 +2,11 @@
 #define _ITEM_NAME_H_
 
 #include <string>
-#include <map>
 
-class ItemName {
-	
-	private:
-	
-		std::map< std::string, std::string> buffer;
-		bool state;
-		
+class ItemName: GeneralItem {
+
 	public:
 	
-		explicit ItemName( const std::string& fileName );
-		void getBuffer() const;
-		unsigned int length();
-		void clear();
 		bool read( std::string& fileName );
 };
 
