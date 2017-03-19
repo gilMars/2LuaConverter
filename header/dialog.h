923096e1be217b322edbeae7b266649566701ed8
gilMars <gilmar.lilium@gmail.com>
2017-03-19 11:31:46 -0300

    Documentação do código

--- header/dialog.h
+++ header/dialog.h
@@ -1,3 +1,8 @@
+/*
+ * Arquivo gerado automaticamente pelo QtCreator. Arquivo de cabeçalho com os dados resposáveis pela GUI.
+ * @author Gilmar B. Freitas.
+ * @version 1.0
+*/
 #ifndef DIALOG_H
 #define DIALOG_H
 
@@ -16,7 +21,18 @@ class Dialog : public QDialog
 public:
     explicit Dialog(QWidget *parent = 0);
     ~Dialog();
+
+    /*
+     * Função membro que inicializa a conversão a conversão dos arquivos em outro thread e manipula outras membros da GUI.
+    */
     void start_convert();
+
+    /*
+     * init_read é o método público que irá receber o diretório dos arquivo e irá carregá-los na memória principal.
+     * @param dir string com o caminho do diretório dos arquivos.
+     * @param log booleano que determina se a função fará log de erros ou não.
+     * @return unsigned int, será um valor inteiro não assinado que representa o arquivo que não pode ser lido, valores entre 1~9.
+    */
     unsigned int init_read(const std::string& dir, bool log);
 
 private slots:
@@ -26,9 +42,11 @@ private slots:
 
     void slot_finished();
 
+    void on_creditos_clicked();
+
 private:
-    QFileDialog diretorio;
-    QFutureWatcher<unsigned int> FutureWatcher;
+    QFileDialog diretorio;                      // Atributo que representa o caminho dos arquivos item_db.conf e etc...
+    QFutureWatcher<unsigned int> FutureWatcher; // Atributo que assiste o thread que fará a conversão do arquivo
     Ui::Dialog *ui;
 };
 
